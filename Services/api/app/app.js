900700e30705dec6eb7ba1eabbb380a11bb5ef17
nunun <hoge.life@gmail.com>
2018-01-31 00:31:35 +0900

    リクエストの受信をサポート

--- Services/api/app/app.js
+++ Services/api/app/app.js
@@ -6,12 +6,17 @@ var mindlinkClient = require('libservices').MindlinkClient.activate();
 // mindlink client
 mindlinkClient.setConfig(config.mindlinkClient);
 mindlinkClient.setLogger(logger.mindlinkClient);
-//mindlinkClient.on('connect', function() {
-//});
-//mindlinkClient.on('disconnect', function() {
-//});
-//mindlinkClient.on('data', function(data) {
-//});
+mindlinkClient.on('request', function(data) {
+    switch (data.type) {
+    case 100: // [server] match start.
+        mindlinkClient.response(data, {param:'hoge'});
+        break;
+    //case 101: // [matching] join user.
+    //  break;
+    //case 102: // [matching] get user.
+    //  break;
+    }
+});
 
 // start app ...
 mindlinkClient.start();
