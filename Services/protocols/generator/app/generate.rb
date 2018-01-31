d5937e1a763250becee704b97565db4cdf8e0537
nunun <hoge.life@gmail.com>
2018-01-31 11:19:00 +0900

    プロトコルジェネレータ実装 (途中)

--- Services/protocols/generator/app/generate.rb
+++ Services/protocols/generator/app/generate.rb
@@ -1,1 +1,12 @@
-p "gen!"
+require 'erb'
+require 'yaml'
+
+spec = YAML.load_file('./spec.yml')
+
+def generate(template_name, spec)
+  models    = spec["models"]
+  templates = spec["templates"]
+  return ERB.new(templates[template_name], nil, "-").result(binding);
+end
+
+puts generate("nodejs", spec);
