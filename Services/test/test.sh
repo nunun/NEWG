a17581fb11f77c2bb6d118c4bbb5716dfc86715b
nunun <hoge.life@gmail.com>
2018-02-21 10:59:13 +0900

    テストの自動検出

--- Services/test/test.sh
+++ Services/test/test.sh
@@ -1,4 +1,8 @@
-task_services() { sh ./tests/services/test.sh ${*}; }
+CWD=$(cd `dirname ${0}`; pwd)
+TESTS=`find "${CWD}/tests" -mindepth 1 -maxdepth 1 -type d | awk -F/ '{print $NF}'`
+for t in ${TESTS}; do
+        eval "task_${t}() { sh ./tests/${t}/test.sh \${*}; }"
+done
 task_down() { for s in `ls ./tests/*/test.sh`; do sh ${s} down; done; }
 task_test() { for s in `ls ./tests/*/test.sh`; do sh ${s} test; sh ${s} down; done; }
 task_build() { for s in `ls ./tests/*/test.sh`; do sh ${s} build; done; }
---
+++ Services/test/tests/unity/test.sh
@@ -0,0 +1,23 @@
+task_up() { task_down; docker-compose up; }
+task_down() { docker-compose down; }
+task_test() {
+        local platform="StandaloneOSX"
+        local xml="/tmp/result.xml"
+        [ "${OSTYPE}" = "cygwin" ] \
+                && platform="StandaloneWindows" \
+                && xml="cygpath -w ${xml}"
+        docker-compose up &
+        unity -runTests -testPlatform "${platform}" -testResults "${xml}"
+}
+task_build() {
+        task_down; docker-compose build --force-rm --pull
+        git submodule update --init --recursive --remote
+        docker pull nunun/mindlink
+        docker-compose run --rm --no-deps matching sh -c \
+                "(cd /usr/local/lib/node_modules/libservices && npm install)"
+        docker-compose run --rm --no-deps matching npm update
+        docker-compose run --rm --no-deps api      npm update
+        docker-compose run --rm --no-deps matching npm link libservices
+        docker-compose run --rm --no-deps api      npm link libservices
+}
+. "`dirname ${0}`/../../../.task.sh" test ${*}
