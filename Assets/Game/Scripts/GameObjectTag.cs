804745985f79cb137dc0dc1e1047f4fdfae6d656
nunun <hoge.life@gmail.com>
2018-03-15 11:53:56 +0900

    ゲームオブジェクトタグ実装

---
+++ Assets/Game/Scripts/GameObjectTag.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲームオブジェクトタグ
+public class GameObjectTag : MonoBehaviour {
+    //------------------------------------------------------------------------- 変数
+    // 検索用タグ名 (Awake 時に確定)
+    string tagName = null;
+
+    // ストック一覧
+    static List<GameObjectTag> gameObjectTags = new List<GameObjectTag>();
+
+    //------------------------------------------------------------------------- 操作
+    protected static GameObjectTag FindGameObjectTag(string tagName, GameObject parent, bool rent) {
+        tagName = tagName ?? typeof(T).Name;
+        for (int i = gameObjectTags.Count - 1; i >= 0; i--) {
+            var gameObjectTag = gameObjectTags[i];
+            if (gameObjectTag.tagName == tagName) {
+                if (parent != null) {
+                    var p = gameObjectTag.gameObject;
+                    while (p != null) {
+                        if (p == parent) {
+                            break;
+                        }
+                        p = p.transform.parent.gameObject;
+                    }
+                    if (p == null) {
+                        continue;
+                    }
+                }
+                if (rent) {
+                    gameObjectTags.Remove(gameObjectTag);
+                }
+                return gameObjectTag.obj as T;
+            }
+        }
+        return default(T);
+    }
+
+    protected static ReturnGameObjectTag(GameObjectTag gameObjectTag) {
+        gameObjectTags.Add(gameObjectTag);
+    }
+
+    //------------------------------------------------------------------------- 実装 (MonoBehaviour)
+    void Awake() {
+        obj = obj ?? this.GetComponent<UIComponent>();
+        Debug.Assert(obj != null, "タグのオブジェクトなし");
+        // NOTE
+        // ひとまず、ゲームオブジェクトタグを貼り付けた
+        // オブジェクト名で検索できるようにしておく。
+        this.tagName = name;
+        gameObjectTags.Add(this);
+    }
+
+    void OnDestroy() {
+        gameObjectTags.Remove(this);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// ゲームオブジェクトタグ (検索用インターフェイス)
+public class GameObjectTag<T> : GameObjectTag where T : Component {
+    //------------------------------------------------------------------------- 貸し出しと返却
+    public static T RentObject(string tagName = null, GameObject parent = null) {
+        tagName = tagName ?? typeof(T).Name;
+        var gameObjectTag = FindGameObjectTag(tagName, parent, true);
+        if (typeof(T) == typeof(GameObjectTag)) {
+            return gameObjectTag as T;
+        }
+        if (gameObjectTag.obj != null) {
+            return gameObjectTag.obj as T;
+        }
+        return gameObjectTag.gameObject.GetComponent<T>();
+    }
+
+    public static T RentObject(GameObject parent) {
+        return RentObject(null, parent);
+    }
+
+    public static void ReturnObject(T instance) {
+        var gameObjectTag = instance.gameObject.GetComponent<GameObjectTag>();
+        Debug.Assert(gameObjectTag != null, "ゲームオブジェクトタグなし");
+        ReturnGameObjectTag(gameObjectTag);
+    }
+
+    //------------------------------------------------------------------------- 検索
+    public static T Find(string tagName = null, GameObject parent = null) {
+        tagName = tagName ?? typeof(T).Name;
+        var gameObjectTag = FindGameObjectTag(tagName, parent, false);
+        if (typeof(T) == typeof(GameObjectTag)) {
+            return gameObjectTag as T;
+        }
+        if (gameObjectTag.obj != null) {
+            return gameObjectTag.obj as T;
+        }
+        return gameObjectTag.gameObject.GetComponent<T>();
+    }
+
+    public static T Find(GameObject parent) {
+        return Find(null, parent);
+    }
+}
