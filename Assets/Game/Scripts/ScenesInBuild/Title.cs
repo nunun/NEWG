919609556908c775c5a3dc814e14be1b4131c344
nunun <hoge.life@gmail.com>
2018-04-13 22:46:52 +0900

    ログイン処理を記述

--- Assets/Game/Scripts/ScenesInBuild/Title.cs
+++ Assets/Game/Scripts/ScenesInBuild/Title.cs
@@ -11,45 +11,78 @@ public class Title : GameScene {
         GameAudio.PlayBGM("Revenge1");
         GameAudio.SetBGMVolume("Revenge1", 1.0f);
 
-        using (var subject = StatusLine.Observe()) {
+        // セットアップ
+        var subject = StatusLine.Observe();
+        try {
+            // 初期化
             subject.message = "初期化中 ...";
             yield return new WaitForSeconds(1.0f);
-            subject.message = "ロード中 ...";
-            yield return new WaitForSeconds(1.0f);
+
+            // サインイン
+            var signinToken = GameDataManager.CredentialData.signinToken;
+            do {
+                subject.message = "サインインしています ...";
+
+                // サインアップしていないならサインイン
+                // そうでないならサインアップ
+                var error = default(string);
+                if (string.IsNullOrEmpty(signinToken)) {
+                    using (var wait = UIWait<WebAPI.SignupResponse>.RentFromPool()) {
+                        WebAPI.Signup("Unnamed Player", wait.Callback);
+                        yield return wait;
+                        error = wait.error;
+                    }
+                } else {
+                    using (var wait = UIWait<WebAPI.SigninResponse>.RentFromPool()) {
+                        WebAPI.Signin(signinToken, wait.Callback);
+                        yield return wait;
+                        error = wait.error;
+                    }
+                }
+                if (error == default(string)) {
+                    break;//成功!
+                }
+                Debug.LogError(error);
+
+                // エラーの場合はリトライ
+                subject.Dispose();
+                using (var wait = UIWait.RentFromPool()) {
+                    OkPopup.Open("ログインに失敗しました。", wait.Callback);
+                    yield return wait;
+                }
+                subject = StatusLine.Observe();
+            } while(true);
+        } finally {
+            subject.Dispose();
         }
 
-        using (var wait = UIWait<WebAPI.SignupResponse>.RentFromPool()) {
-            WebAPI.Signup("i_am_tester", wait.Callback);
-            yield return wait;
-            Debug.Log(wait.error);
-        }
+        // TODO
+        // サインイン結果
         Debug.Log(GameDataManager.PlayerData.playerName);
 
-        using (var wait = UIWait.RentFromPool()) {
-            var popup = MessagePopup.Open("これはメッセージポップアップです。", wait.Callback);
-            yield return new WaitForSeconds(2.0f);
-            popup.Close();
-            yield return wait;
-            Debug.Log(wait.error);
-        }
-
-        using (var wait = UIWait.RentFromPool()) {
-            OkPopup.Open("これは OK ポップアップです。", wait.Callback, "がんばるぞい");
-            yield return wait;
-            Debug.Log(wait.error);
-        }
-
-        using (var wait = UIWait<bool>.RentFromPool()) {
-            YesNoPopup.Open("これは OK ポップアップです。", wait.Callback, "やる", "やめた");
-            yield return wait;
-            Debug.Log(wait.error);
-            Debug.Log(wait.Value1);
-        }
-
+        // セットアップ完了！
         //GameAudio.MixBGM("Revenge2");
     }
 }
 
+//using (var wait = UIWait.RentFromPool()) {
+//    var popup = MessagePopup.Open("これはメッセージポップアップです。", wait.Callback);
+//    yield return new WaitForSeconds(2.0f);
+//    popup.Close();
+//    yield return wait;
+//    Debug.Log(wait.error);
+//}
+//using (var wait = UIWait.RentFromPool()) {
+//    OkPopup.Open("これは OK ポップアップです。", wait.Callback, "がんばるぞい");
+//    yield return wait;
+//    Debug.Log(wait.error);
+//}
+//using (var wait = UIWait<bool>.RentFromPool()) {
+//    YesNoPopup.Open("これは OK ポップアップです。", wait.Callback, "やる", "やめた");
+//    yield return wait;
+//    Debug.Log(wait.error);
+//    Debug.Log(wait.Value1);
+//}
 //[SerializeData]
 //public class MyData {
 //    public int val = 0;
