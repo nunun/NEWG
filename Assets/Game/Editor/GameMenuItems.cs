160e923b3e61b98e1bd0ccadded68bf20449cb53
nunun <hoge.life@gmail.com>
2018-04-15 23:33:55 +0900

    タイトル画面作りこみ

--- Assets/Game/Editor/GameMenuItems.cs
+++ Assets/Game/Editor/GameMenuItems.cs
@@ -128,4 +128,57 @@ public partial class GameMenuItems {
         PlayerPrefs.Save();
         Debug.Log("セーブデータをクリアしました。");
     }
+
+    //-------------------------------------------------------------------------- デバッグ
+    [MenuItem("Game/編集/uGUI/アンカーを現在位置にセット &]", false, 104)]
+    public static void SetAnchorToCurrentPosition() {
+        var o = Selection.activeGameObject;
+        if (o != null && o.GetComponent<RectTransform>() != null) {
+            var p = o.transform.parent.GetComponent<RectTransform>();
+            var r = o.GetComponent<RectTransform>();
+
+            var parentWidth  = p.rect.width;
+            var parentHeight = p.rect.height;
+
+            var offsetMin = r.offsetMin;
+            var offsetMax = r.offsetMax;
+            var anchorMin = r.anchorMin;
+            var anchorMax = r.anchorMax;
+
+            var targetAnchorMin = new Vector2(anchorMin.x + (offsetMin.x / parentWidth), anchorMin.y + (offsetMin.y / parentHeight));
+            var targetAnchorMax = new Vector2(anchorMax.x + (offsetMax.x / parentWidth), anchorMax.y + (offsetMax.y / parentHeight));
+
+            r.anchorMin = targetAnchorMin;
+            r.anchorMax = targetAnchorMax;
+            r.offsetMin = new Vector2(0, 0);
+            r.offsetMax = new Vector2(1, 1);
+            r.pivot     = new Vector2(0.5f, 0.5f);
+        }
+    }
+
+    [MenuItem("Game/編集/uGUI/アンカーを中心にセット &^", false, 105)]
+    public static void SetAnchorToCenterPosition() {
+        var o = Selection.activeGameObject;
+        if (o != null && o.GetComponent<RectTransform>() != null) {
+            var p = o.transform.parent.GetComponent<RectTransform>();
+            var r = o.GetComponent<RectTransform>();
+
+            var parentWidth  = p.rect.width;
+            var parentHeight = p.rect.height;
+
+            var x      = r.position.x;
+            var y      = r.position.y;
+            var width  = r.rect.width;
+            var height = r.rect.height;
+
+            var targetAnchorMin = new Vector2(x / parentWidth, y / parentHeight);
+            var targetAnchorMax = new Vector2(x / parentWidth, y / parentHeight);
+
+            r.anchorMin = targetAnchorMin;
+            r.anchorMax = targetAnchorMax;
+            r.offsetMin = new Vector2(-width / 2.0f, -height / 2.0f);
+            r.offsetMax = new Vector2( width / 2.0f,  height / 2.0f);
+            r.pivot     = new Vector2(0.5f, 0.5f);
+        }
+    }
 }
---
+++ Assets/Game/Scripts/UI/MultilineBestFit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 複数行で適切なフォントサイズを決める
+public class MultilineBestFit : MonoBehaviour {
+    //-------------------------------------------------------------------------- インスタンスの確保と解放
+    [SerializeField] float rows = 2.0f;
+
+    //-------------------------------------------------------------------------- 実装 (MonoBehaviour)
+    void Awake() {
+        var rectTransform = GetComponent<RectTransform>();
+        var text          = GetComponent<Text>();
+
+        Debug.Assert(rectTransform != null, "RectTransform なし");
+        Debug.Assert(text          != null, "Text なし");
+
+        text.fontSize = (int)(((rectTransform.rect.height / rows) - text.lineSpacing) / 1.7f);
+    }
+}
