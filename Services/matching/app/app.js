b7ad21f1779b1e122db755534d75a82a3aba8281
nunun <hoge.life@gmail.com>
2018-05-16 13:06:33 +0900

    マッチング処理作り込み

--- Services/matching/app/app.js
+++ Services/matching/app/app.js
@@ -7,6 +7,7 @@ var couchClient              = require('./services/library/couch_client').activa
 var redisClient              = require('./services/library/redis_client').activate(config.redisClient, logger.redisClient);
 var mindlinkClient           = require('./services/library/mindlink_client').activate(config.mindlinkClient, logger.mindlinkClient);
 var matchingServer           = require('./services/library/websocket_server').activate(config.matchingServer, logger.matchingServer);
+var UserData                 = models.UserData;
 var MatchingData             = models.MatchingData;
 var MatchConnectData         = models.MatchConnectData;
 var MatchingServerStatusData = models.MatchingServerStatusData;
@@ -54,51 +55,139 @@ matchingServer.setAcceptEventListener(function(req) {
     if (!location.query.matchingId) {
         return null;
     }
-
     // マッチングID を確認
     return {matchingId:location.query.matchingId};
 });
 matchingServer.setConnectEventListener(function(matchingClient) {
-    var matchingId = matchingClient.acceptData.matchingId;
-    MatchingData.getCache(matchingId, (err,data) => {
-        // NOTE
-        // ユーザ特定
-        // 本来であればユーザをチェックして難易度別にサーバを選択。
-        // 今はシンプルマッチングなのでやらない。
-        //var userId = data.users[0];
-
-        // シンプルマッチング
-        // サービス一覧からゲームサーバをとって返却
-        var cond = ".*{.alias == \"server\" && .serverState == \"ready\" && .load < 1.0}";
-        mindlinkClient.sendQuery(cond, function(err,services) {
-            if (err) {
-                res.send({err:err.toString()});
-                return;
-            }
-            if (!services || services.length <= 0) {
-                res.send({err:new Error('no server found.')});
-                return;
-            }
-            var service = services[0];
-            logger.mindlinkClient.debug('server found: service[' + util.inspect(service, {depth:null,breakLength:Infinity}) + ']');
-
-            // サーバ接続情報を返却
-            var matchConnectData = new MatchConnectData();
-            matchConnectData.serverAddress = service.serverAddress;
-            matchConnectData.serverPort    = service.serverPort;
-            matchConnectData.matchId       = null; // NOTE シンプルマッチングのため必要無し
-            matchingClient.send(0, matchConnectData);
-
-            // 返却後 2 秒で切断
-            setTimeout(() => {
-                matchingClient.stop();
-            }, 2000);
-        });
-    });
+    matchingAccept(matchingClient);
 });
 matchingServer.setDisconnectEventListener(function(matchingClient) {
-    mindlinkClient.cancelRequest(matchingClient.requestId);
+    matchingCancel(matchingClient);
 });
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// マッチングキュー
+var matchingQueue = [];
+
+// マッチングタイマー
+var matchingTimerId = 0;
+
+// ユーザを受け入れ
+function matchingAccept(matchingClient, matchingId) {
+    MatchingData.getCache(matchingClient.acceptData.matchingId, (err,data) => {
+        if (err) {
+            matchingAbort(matchingClient, new Error("cache error."));
+            return;
+        }
+        if (!data) {
+            matchingAbort(matchingClient, new Error("invalid matchingId."));
+            return;
+        }
+        matchingClient.userId = users[0];
+        matchingIdentify(matchingClient);
+    });
+}
+
+// ユーザを識別
+function matchingIdentify(matchingClient) {
+    UserData.get(matchingClient.userId, (err,userData) => {
+        if (err) {
+            matchingAbort(matchingClient, err);
+            return;
+        }
+        if (!userData) {
+            matchingAbort(matchingClient, new Error("invalid user."));
+            return;
+        }
+        matchingClient.userData = userData;
+        matchingEnqueue(matchingClient);
+    });
+}
+
+// ユーザをキューに追加
+function matchingEnqueue(matchingClient) {
+    // キューに追加
+    matchingQueue.push(matchingClient);
+
+    // 更新ループの起動
+    if (!matchingTimerId) {
+        setInterval(matchingUpdate, 500); // NOTE 500ms で更新する
+    }
+}
+
+// マッチング処理
+function matchingUpdate() {
+    // TODO
+    // 更新処理
+}
+
+// マッチング中断
+function matchingAbort(matchingClient, err) {
+    // 即キャンセル
+    matchingCancel(matchingClient);
+
+    // メッセージ送信
+    matchingClient.send(0, {err:err});
+
+    // 返却後 2 秒で切断
+    setTimeout(() => {
+        matchingClient.stop();
+    }, 2000);
+}
+
+// マッチングキャンセル
+function matchingCancel(matchingClient) {
+    // マッチングキューから外す
+    for (i = matchingQueue.length - 1; i >= 0; i--) {
+        if (matchingQueue[i] == matchingClient) {
+            matchingQueue.splice(i, 1);
+        }
+    }
+
+    // NOTE
+    // 今は必要ないのでコメントアウト
+    //mindlinkClient.cancelRequest(matchingClient.requestId);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // start app ...
 couchClient.start();
+
+// NOTE
+// ユーザ特定
+// 本来であればユーザをチェックして難易度別にサーバを選択。
+// 今はシンプルマッチングなのでやらない。
+//var userId = data.users[0];
+// シンプルマッチング
+// サービス一覧からゲームサーバをとって返却
+// var cond = ".*{.alias == \"server\" && .serverState == \"ready\" && .load < 1.0}";
+// mindlinkClient.sendQuery(cond, function(err,services) {
+//     if (err) {
+//         res.send({err:err.toString()});
+//         return;
+//     }
+//     if (!services || services.length <= 0) {
+//         res.send({err:new Error('no server found.')});
+//         return;
+//     }
+//     var service = services[0];
+//     logger.mindlinkClient.debug('server found: service[' + util.inspect(service, {depth:null,breakLength:Infinity}) + ']');
+//
+//     // サーバ接続情報を返却
+//     var matchConnectData = new MatchConnectData();
+//     matchConnectData.serverAddress = service.serverAddress;
+//     matchConnectData.serverPort    = service.serverPort;
+//     matchConnectData.matchId       = null; // NOTE シンプルマッチングのため必要無し
+//     matchingClient.send(0, matchConnectData);
+//
+//     // 返却後 2 秒で切断
+//     setTimeout(() => {
+//         matchingClient.stop();
+//     }, 2000);
+// });
