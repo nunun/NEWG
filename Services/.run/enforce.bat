b70f678074b8826e4c41358cf80d4e8ace5c6d55
nunun <hoge.life@gmail.com>
2018-05-23 00:00:13 +0900

    enforce 作りこみ

--- Services/.run/enforce.bat
+++ Services/.run/enforce.bat
@@ -1,4 +1,4 @@
-docker run --privileged --rm -w /work -e TERM=xterm ^
+docker run --privileged --rm -it -w /work -e TERM=xterm ^
         -v /var/run/docker.sock:/var/run/docker.sock ^
         -v %cd%:/work ^
         nunun/enforce %1 %2 %3 %4 %5 %6 %7 %8 %9
