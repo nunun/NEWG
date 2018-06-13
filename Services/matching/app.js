5feccd28f3e2f4870b0f0b8120e396e0057f58d1
nunun <hoge.life@gmail.com>
2018-06-14 00:58:16 +0900

    マッチング整理 ユーザをロックしてからサーバをセットアップする サーバのセットアップが失敗したらロックを解除してやり直す

--- Services/matching/app.js
+++ Services/matching/app.js
@@ -55,7 +55,7 @@ mindlinkClient.setDisconnectEventListener(function() {
     stopMatchingBrainSupply();
 });
 mindlinkClient.setDataFromRemoteEventListener(0, (data,res) => {
-    var task = setupQueue.getTaskAtKey(data.matchId);
+    var task = matchingBrainQueue.getTaskAtKey(data.matchId);
     if (!task) {
         var errorMessage = 'matchId not found? (' + data.matchId + ')';
         logger.matchingServer.debug(errorMessage);
@@ -117,12 +117,10 @@ joinQueue.setAbortEventListener((err, task) => {
 
 // ユーザ識別
 async function identifyUser(task) {
-    joinQueue.logger.debug("identify user.");
-
-    // 次のキューがフルなら待ち
     if (matchingQueue.isFull()) {
-        return 1000;
+        return 1000;//次のキューがフルなら待ち
     }
+    joinQueue.logger.debug("identify user.");
 
     // 初期化
     var matchingClient = task._key;
@@ -158,6 +156,8 @@ async function identifyUser(task) {
 
 // マッチングキュー
 // マッチング待ちのユーザのキューです。
+// 基本的に何もしません。
+// マッチングブレインにより、マッチングされるのを待ちます。
 var matchingQueue = new TaskQueue(config.matchingQueue, logger.matchingQueue);
 matchingQueue.setAddEventListener((task) => {
     matchingQueue.logger.debug("task added.");
@@ -166,7 +166,6 @@ matchingQueue.setAddEventListener((task) => {
 matchingQueue.setAbortEventListener((err, task) => {
     joinQueue.logger.debug("task aborted (" + err + ").");
     task.err = err;
-    matchingQueue.remove(task);
     errorQueue.add(task);
 });
 
@@ -174,221 +173,8 @@ matchingQueue.setAbortEventListener((err, task) => {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-// マッチングブレインID生成用
-var matchingBrainIdCounter = 0;
-
-// マッチID生成用
-var matchIdCounter = 0;
-
-// マッチングブレイン供給タイマー
-var matchingBrainSupplyTimer = null;
-
-// NOTE
-// マッチングブレイン供給開始
-// マインドリンクに接続した時点で開始する。
-// 定期的にマッチングブレインを供給し続ける。
-function startMatchingBrainSupply() {
-    if (matchingBrainSupplyTimer) {
-        matchingBrainQueue.logger.error("matchingBrainSupplyTimer is already started.");
-        return;
-    }
-    matchingBrainQueue.logger.debug("start matchingBrainSupplyTimer ...");
-    matchingBrainSupplyTimer = setInterval(() => {
-        if (!matchingBrainQueue.isFull()) {
-            matchingBrainQueue.logger.debug("supplying new matchingBrain ...");
-            var matchingBrainTask = TaskQueue.createTaskFromKey(++matchingBrainIdCounter);
-            matchingBrainQueue.add(matchingBrainTask);
-        }
-    }, 1000);
-}
-
-// NOTE
-// マッチングブレイン供給停止
-// マインドリンクから切断した時点で停止される。
-function stopMatchingBrainSupply() {
-    if (!matchingBrainSupplyTimer) {
-        matchingBrainQueue.logger.error("matchingBrainSupplyTimer does not started yet.");
-        return;
-    }
-    matchingBrainQueue.logger.debug("stop matchingBrainSupplyTimer ...");
-    stopInterval(matchingBrainSupplyTimer);
-    matchingBrainQueue.logger.debug("remove all matchingBrain ...");
-    var matchingBrainTask = null;
-    while (matchingBrainTask = matchingBrainQueue.getTaskAt(0)) {
-        matchingBrainQueue.remove(matchingBrainTask);
-    }
-}
-
-// マッチングブレインキュー
-// 参加できそうなサーバに目星を付けた後、
-// マッチングキューを監視してユーザのマッチングを行い、
-// マッチングしたユーザに結果を通知します。
-var matchingBrainQueue = new TaskQueue(config.matchingBrainQueue, logger.matchingBrainQueue);
-matchingBrainQueue.setAddEventListener((task) => {
-    matchingBrainQueue.logger.debug("task added.");
-    return findServer; // NOTE サーバ探しから始める
-});
-matchingBrainQueue.setAbortEventListener((err, task) => {
-    matchingBrainQueue.logger.debug("task aborted (" + err + ").");
-    task.err = err;
-    matchingBrainQueue.remove(task);
-    setupErrorQueue.add(task);
-});
-
-// 参加できそうなサーバを探す
-function findServer(task) {
-    matchingBrainQueue.logger.debug("find server.");
-    return new Promise((resolved, reject) => {
-        // NOTE
-        // 現在は立ち上がっているサーバには入れるようにしておく。
-        // 空きサーバに所定のメンバーを入れる場合は、
-        // サーバ参加確認と参加応答のプロトコルを組み込む必要がある。
-        // これはデータベース経由で実現することも可能なので、どの実装が良いかは今後検討する。
-        //
-        // NOTE
-        // 現在の実装では、マッチングブレインがセットアップキューに送り出されると
-        // 次のマッチングブレインが "stanby" サーバを掴むので、マッチングが成立してしまう点に注意。
-        var cond = ".*{.alias == \"server\" && (.serverState == \"standby\" || .serverState == \"ready\") && .load < 1.0}";
-        mindlinkClient.sendQuery(cond, function(err,services) {
-            if (err) {
-                resolved(10000);//送信エラー(10秒待ってリトライ)
-                return;
-            }
-            if (!services || services.length <= 0) {
-                resolved(3000);//サービス無し(3秒待ってリトライ)
-                return;
-            }
-            task.service = services[0];
-            matchingBrainQueue.logger.debug('server found: service[' + util.inspect(task.service, {depth:null,breakLength:Infinity}) + ']');
-            resolved(makeMatching);
-        });
-    });
-}
-
-// マッチングする
-async function makeMatching(task) {
-    matchingBrainQueue.logger.debug("make matching.");
-
-    // 次のキューがフルならしばらく待ち
-    if (setupQueue.isFull()) {
-        return 1000;
-    }
-    // マッチングが成立するためのユーザいない
-    if (matchingQueue.getLength() <= 0) {
-        return 1000;
-    }
-    var matchingKeys    = [];
-    var matchingClients = [];
-    var matchingUsers   = [];
-
-    // NOTE
-    // 今は上から順にマッチング
-    // そのうち上等なマッチングのロジックに治す。
-    var matchingTask = matchingQueue.getTaskAt(0);
-    matchingKeys.push(matchingTask._key);
-    matchingClients.push(matchingTask._key);
-    matchingUsers.push(matchingTask.userId);
-    matchingQueue.remove(matchingTask);
-
-    // サーバ セットアップ リクエストメッセージ
-    var serverSetupRequestMessage = new ServerSetupRequestMessage();
-    serverSetupRequestMessage.matchId       = (++matchIdCounter).toString();
-    serverSetupRequestMessage.sceneName     = "MapProvingGround"; // TODO
-    serverSetupRequestMessage.matchingUsers = matchingUsers;
-    matchingKeys.push(serverSetupRequestMessage.matchId);
-
-    // セットアップタスクを作成して投入
-    var setupTask = TaskQueue.createTaskFromKey(matchingKeys);
-    setupTask.service                   = task.service;
-    setupTask.matchingClients           = matchingClients;
-    setupTask.serverSetupRequestMessage = serverSetupRequestMessage;
-    matchingBrainQueue.remove(task);
-    setupQueue.add(setupTask);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-// セットアップキュー
-// サーバを起動したりデータベースに書き込んだりした後
-// ユーザにマッチング結果を通知します。
-var setupQueue = new TaskQueue(config.setupQueue, logger.setupQueue);
-setupQueue.setAddEventListener((task) => {
-    setupQueue.logger.debug("task added.");
-    return setupRequest;
-});
-setupQueue.setAbortEventListener((err, task) => {
-    setupQueue.logger.debug("task aborted (" + err + ").");
-    task.err = err;
-    setupQueue.remove(task);
-    setupErrorQueue.add(task);
-});
-
-// セットアップリクエストを飛ばす
-async function setupRequest(task) {
-    setupQueue.logger.debug("setup request.");
-    mindlinkClient.sendToRemote(task.service.clientUuid, 0, task.serverSetupRequestMessage);
-    return waitForSetupResponse;
-}
-
-// セットアップレスポンス待ち...
-async function waitForSetupResponse(task) {
-    if (task._count == 0) {
-        setupQueue.logger.debug("wait for setup response.");
-        task.waitCount = 0;
-    }
-
-    // メッセージ来た？
-    if (!task.serverSetupDoneMessage) {
-        if (task.waitCount++ >= 30) {
-            throw new Error('setup wait count exceeded.');
-        }
-        return 1000;
-    }
-
-    // セットアップが完了したらしいので
-    // マッチ完了を通知
-    var matchConnectData = new MatchConnectData();
-    matchConnectData.serverAddress = task.service.serverAddress;
-    matchConnectData.serverPort    = task.service.serverPort;
-    matchConnectData.matchId       = task.serverSetupDoneMessage.matchId;
-    task.matchConnectData = matchConnectData;
-    return sendMatchConnectData;
-}
-
-// セットアップアイテムに記録された全ユーザにマッチ完了を通知
-async function sendMatchConnectData(task) {
-    setupQueue.logger.debug("send match connect data.");
-    var matchingClients = task.matchingClients;
-    for (var i in matchingClients) {
-        var matchingClient = matchingClients[i];
-        matchingClient.send(0, task.matchConnectData);
-    }
-    return waitForSetupDisconnect;
-}
-
-// 数秒待って全て閉じる
-async function waitForSetupDisconnect(task) {
-    if (task._count == 0) {
-        setupQueue.logger.debug("wait for setup disconnect.");
-        return 3000;
-    }
-    var matchingClients = task.matchingClients;
-    for (var i in matchingClients) {
-        var matchingClient = matchingClients[i];
-        matchingClient.stop();
-    }
-    setupQueue.remove(task);
-    return null;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
 // エラーキュー
-// エラーを送信して数秒後に切断します。
+// エラーを送信して数秒後に切断するキューです。
 var errorQueue = new TaskQueue(config.errorQueue, logger.errorQueue);
 errorQueue.setAddEventListener((task) => {
     errorQueue.logger.debug("task added.");
@@ -423,42 +209,200 @@ async function waitForErrorDisconnect(task) {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-// セットアップエラーキュー
-// データベースにエラーを書き込んだりした後、
-// ユーザにマッチング失敗を通知します。
-var setupErrorQueue = new TaskQueue(config.setupErrorQueue, logger.setupErrorQueue);
-setupErrorQueue.setAddEventListener((task) => {
-    setupErrorQueue.logger.debug("task added.");
-    return sendSetupError;
-});
-setupErrorQueue.setAbortEventListener((err, task) => {
-    setupErrorQueue.logger.debug("task aborted (" + err + ").");
-    setupErrorQueue.remove(task);
-});
+// マッチID生成用
+var matchIdCounter = 0;
 
-// セットアップエラー送信
-async function sendSetupError(task) {
-    setupErrorQueue.logger.debug("send setup error.");
-    var matchingClients = task.matchingClients;
-    for (var i in matchingClients) {
-        var matchingClient = matchingClients[i];
-        matchingClient.send(1, task.err);
+// マッチングブレイン供給タイマー
+var matchingBrainSupplyTimer = null;
+
+// NOTE
+// マッチングブレイン供給開始
+// マインドリンクに接続した時点で開始する。
+// 定期的にマッチングブレインを供給し続ける。
+function startMatchingBrainSupply() {
+    if (matchingBrainSupplyTimer) {
+        matchingBrainQueue.logger.error("matchingBrainSupplyTimer is already started.");
+        return;
     }
-    return waitForSetupErrorDisconnect;
+    matchingBrainQueue.logger.debug("start matchingBrainSupplyTimer ...");
+    matchingBrainSupplyTimer = setInterval(() => {
+        if (!matchingBrainQueue.isFull()) {
+            matchingBrainQueue.logger.debug("supplying new matchingBrain ...");
+            var matchingBrainTask = TaskQueue.createTaskFromKey(++matchIdCounter);
+            matchingBrainQueue.add(matchingBrainTask);
+        }
+    }, 1000);
 }
 
-// 数秒まってキューから消す
-async function waitForSetupErrorDisconnect(task) {
+// NOTE
+// マッチングブレイン供給停止
+// マインドリンクから切断した時点で停止される。
+function stopMatchingBrainSupply() {
+    if (!matchingBrainSupplyTimer) {
+        matchingBrainQueue.logger.error("matchingBrainSupplyTimer does not started yet.");
+        return;
+    }
+    matchingBrainQueue.logger.debug("stop matchingBrainSupplyTimer ...");
+    stopInterval(matchingBrainSupplyTimer);
+    matchingBrainQueue.logger.debug("remove all matchingBrain ...");
+    var matchingBrainTask = null;
+    while (matchingBrainTask = matchingBrainQueue.getTaskAt(0)) {
+        matchingBrainQueue.remove(matchingBrainTask);
+    }
+}
+
+// マッチングブレインキュー
+// マッチングキューを監視してユーザのマッチングを行い、
+// 参加できそうなサーバに目星を付けた後、サーバのセットアップを行い
+// マッチング結果をユーザに通知します。
+var matchingBrainQueue = new TaskQueue(config.matchingBrainQueue, logger.matchingBrainQueue);
+matchingBrainQueue.setAddEventListener((task) => {
+    matchingBrainQueue.logger.debug("task added.");
+    return makeMatching;
+});
+matchingBrainQueue.setAbortEventListener((err, task) => {
+    matchingBrainQueue.logger.debug("task aborted (" + err + ").");
+    abortMatching(task);
+});
+
+// マッチング
+async function makeMatching(task) {
+    if (matchingQueue.getLength() <= 0) {
+        return 1000;//マッチングが成立するためのユーザいない
+    }
+    matchingBrainQueue.logger.debug("make matching.");
+
+    // NOTE
+    // マッチングするユーザを探す。
+    // 今はマッチングキューの先頭からひとつずつ取る。
+    var matchingTasks = [];
+    var l = matchingQueue.getLength();
+    for (var i = 0; i < l; i++) {
+        var matchingTask = matchingQueue.getTaskAt(i);
+        if (matchingTask.lockId) {
+            if (matchingBrainTask.indexOfKey(matchingTask.lockId) < 0) {
+                matchingTask.lockId = null;//NOTE ロックID無効につき解除
+            }
+        }
+        if (!matchingTask.lockId) {
+            matchingTask.lockId = task._key;//NOTE ロック
+            matchingTasks.push(matchingTask);
+            break;
+        }
+    }
+
+    // マッチングできるユーザが見つからない
+    if (matchingTasks.length <= 0) {
+        return 1000;
+    }
+
+    // ユーザID一覧作成
+    var matchingUsers = [];
+    for (var i in matchingTasks) {
+        var matchingTask = matchingTasks[i];
+        matchingUsers.push(matchingTask.userId);
+    }
+
+    // セットアップリクエスト準備
+    var serverSetupRequestMessage = new ServerSetupRequestMessage();
+    serverSetupRequestMessage.matchId       = task._key;
+    serverSetupRequestMessage.sceneName     = "MapProvingGround"; // TODO
+    serverSetupRequestMessage.matchingUsers = matchingUsers;
+
+    // セットアップ開始
+    task.matchingTasks             = matchingTasks;
+    task.serverSetupRequestMessage = serverSetupRequestMessage;
+    return findServer;
+}
+
+// 参加できそうなサーバを探す
+function findServer(task) {
+    matchingBrainQueue.logger.debug("find server.");
+    return new Promise((resolved, reject) => {
+        var cond = ".*{.alias == \"server\" && (.serverState == \"standby\" || .serverState == \"ready\") && .load < 1.0}";
+        mindlinkClient.sendQuery(cond, function(err,services) {
+            if (err) {
+                resolved(abortMatching);//送信エラー
+                return;
+            }
+            if (!services || services.length <= 0) {
+                resolved(abortMatching);//サービス無し
+                return;
+            }
+            task.service = services[0];
+            matchingBrainQueue.logger.debug('server found: service[' + util.inspect(task.service, {depth:null,breakLength:Infinity}) + ']');
+            resolved(setupRequest);
+        });
+    });
+}
+
+// セットアップリクエストを飛ばす
+async function setupRequest(task) {
+    matchingBrainQueue.logger.debug("setup request.");
+    mindlinkClient.sendToRemote(task.service.clientUuid, 0, task.serverSetupRequestMessage);
+    return waitForSetupResponse;
+}
+
+// セットアップレスポンス待ち...
+async function waitForSetupResponse(task) {
     if (task._count == 0) {
-        setupErrorQueue.logger.debug("wait for setup error disconnect.");
+        matchingBrainQueue.logger.debug("wait for setup response.");
+        task.waitCount = 0;
+    }
+
+    // メッセージ来た？
+    if (!task.serverSetupDoneMessage) {
+        if (task.waitCount++ >= 30) {
+            return abortMatching;
+        }
+        return 1000;
+    }
+
+    // セットアップが完了したらしいので
+    // 各ユーザにマッチ完了を通知
+    var matchConnectData = new MatchConnectData();
+    matchConnectData.serverAddress = task.service.serverAddress;
+    matchConnectData.serverPort    = task.service.serverPort;
+    matchConnectData.matchId       = task.serverSetupDoneMessage.matchId;
+    task.matchConnectData = matchConnectData;
+    return sendMatchConnectData;
+}
+
+// 各ユーザにマッチ完了を通知
+async function sendMatchConnectData(task) {
+    matchingBrainQueue.logger.debug("send match connect data.");
+    var matchingTasks = task.matchingTasks;
+    for (var i in matchingTasks) {
+        var matchingTask = matchingTasks[i];
+        matchingTask._key.send(0, task.matchConnectData);
+    }
+    return waitForMatchingDisconnect;
+}
+
+// 数秒待って閉じる
+async function waitForMatchingDisconnect(task) {
+    if (task._count == 0) {
+        matchingBrainQueue.logger.debug("wait for setup disconnect.");
         return 3000;
     }
-    var matchingClients = task.matchingClients;
-    for (var i in matchingClients) {
-        var matchingClient = matchingClients[i];
-        matchingClient.stop();
+    var matchingTasks = task.matchingTasks;
+    for (var i in matchingTasks) {
+        var matchingTask = matchingTasks[i];
+        matchingTask._key.stop();
+        matchingQueue.remove(matchingTask);
     }
-    setupQueue.remove(task);
+    matchingBrainQueue.remove(task);
+    return null;
+}
+
+// マッチング中断
+async function abortMatching(task) {
+    var matchingTasks = task.matchingTasks;
+    for (var i in matchingTasks) {
+        var matchingTask = matchingTasks[i];
+        matchingTask.lockId = null;//NOTE ロック解除
+    }
+    matchingBrainQueue.remove(task);
     return null;
 }
 
